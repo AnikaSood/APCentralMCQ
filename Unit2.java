68e41504bc1d9273590547de5649afdf377e4adb
Anika Sood <soodanika@gmail.com>
2020-03-13 11:28:16 -0700

    asd

--- Unit2.java
+++ Unit2.java
@@ -1,9 +1,19 @@
+
+
 public class Unit2
 {
     public static void main()
     {
+        //QUESTION 1  
         Thing someThing; 
-        System.out.println("D. " + someThing.toString()); 
+        //System.out.println("D. " + someThing.toString()); //this causes an error when uncommented because
+        //the someThing object is not initialized and contains null
+     
+        //QUESTION 2   
+        int val = (int)(Math.random() * 4 + 1); //this is choice d, and it works. other options do 
+        //not satisfy the condition
+        val *= 2;
+        System.out.println(val);
     }
     
 }
---
+++ Unit3.java
@@ -0,0 +1,11 @@
+public class Unit3
+{
+    public static void main(int temp)
+    {
+        String weather;
+        if (temp <= 31)
+        {
+            weather = "cold";
+        }
+    }
+}
